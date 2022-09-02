d1811bdb630f91bf19ee5a67e19168291ca4b2a1
stlmx <1291100135@qq.com>
2022-09-02 19:35:02 +0800

    增加注释

--- mdistiller/distillers/KD.py
+++ mdistiller/distillers/KD.py
@@ -4,9 +4,11 @@ import torch.nn.functional as F
 
 from ._base import Distiller
 
-
+# 这个地方对student做log处理是没有问题的，是为了防止计算下溢，具体细节：https://pytorch.org/docs/stable/generated/torch.nn.KLDivLoss.html#torch.nn.KLDivLoss
 def kd_loss(logits_student, logits_teacher, temperature):
     log_pred_student = F.log_softmax(logits_student / temperature, dim=1)
+    # logits_student的shape为[batchsize, 100], 温度默认为4
+    # F.log_softmax就是先做softmax，经过log_softmax后shape不变
     pred_teacher = F.softmax(logits_teacher / temperature, dim=1)
     loss_kd = F.kl_div(log_pred_student, pred_teacher, reduction="none").sum(1).mean()
     loss_kd *= temperature**2
@@ -28,10 +30,10 @@ class KD(Distiller):
             logits_teacher, _ = self.teacher(image)
 
         # losses
-        loss_ce = self.ce_loss_weight * F.cross_entropy(logits_student, target)
+        loss_ce = self.ce_loss_weight * F.cross_entropy(logits_student, target) # 学生和标签的误差，权重0.1
         loss_kd = self.kd_loss_weight * kd_loss(
             logits_student, logits_teacher, self.temperature
-        )
+        ) # 学生和老师的误差，权重0.9
         losses_dict = {
             "loss_ce": loss_ce,
             "loss_kd": loss_kd,
